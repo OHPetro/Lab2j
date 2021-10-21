7ef18670776b870d273f074e006685a72fddd2f6
OHpetro <petiamatiu9@gmail.com>
2021-10-21 21:50:22 +0300

    step5

--- src/com/company/Cal1.java
+++ src/com/company/Cal1.java
@@ -4,14 +4,28 @@ public class Cal1 {
     public static void Add(String str){
 
         int res = 0;
-
         str = str.replaceAll("\\\\n",",");
+
+        if(str.contains("-") == true) {
+            System.out.println("\nВы ввели отрицательные числа ");
+
+            String [] minus = str.split("(?=-)|\\D");
+            for(int a=0;a<minus.length;a++) {
+                if(minus[a].contains("-")){
+                    System.out.println(minus[a]);
+                }
+
+            }
+            System. exit(0);
+
+        }
+
 
 
         if(str.matches("^([0-9]*+\\W*)*$") == false){ //
             System.out.println("Ваш ввод был неправельным ");
         }else{
-            String[] value = str.split("\\W*"); //разделяем строку по ","
+            String[] value = str.split("\\W"); //разделяем строку по ","
             for(int i=0;i<value.length;i++){
                 int strtoint = 0;
                 try{
