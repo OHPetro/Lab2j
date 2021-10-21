bdc3b9eb991ec049047411f34d95a30bca8236fd
OHpetro <petiamatiu9@gmail.com>
2021-10-21 20:19:50 +0300

    step4

--- src/com/company/Cal1.java
+++ src/com/company/Cal1.java
@@ -4,12 +4,14 @@ public class Cal1 {
     public static void Add(String str){
 
         int res = 0;
+
         str = str.replaceAll("\\\\n",",");
 
-        if(str.matches("^([0-9]*+,*)*$") == false){ //
+
+        if(str.matches("^([0-9]*+\\W*)*$") == false){ //
             System.out.println("Ваш ввод был неправельным ");
         }else{
-            String[] value = str.split(","); //разделяем строку по ","
+            String[] value = str.split("\\W*"); //разделяем строку по ","
             for(int i=0;i<value.length;i++){
                 int strtoint = 0;
                 try{
